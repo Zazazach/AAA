12374ee72c7b6b04e9525243491f8ff3a083ae13
霍柏贺 <huobohe@huobohedeMacBook-Pro.local>
2017-04-18 19:36:45 +0800

    注册

--- MyApplication11/app/src/main/java/com/example/huobohe/myapplication11/MainActivity.java
+++ MyApplication11/app/src/main/java/com/example/huobohe/myapplication11/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //11122333
+        //注册写完了
 
     }
 }
